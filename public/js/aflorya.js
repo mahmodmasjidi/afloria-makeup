db528f40f514e5f14feba94db29f60fd930af067
Mahmod Masjidi <mahmod.masjidi@gmail.com>
2025-08-27 15:17:35 +0300

    index.html, aflorya.css ve aflorya.js dosyaları güncellendi

--- public/js/aflorya.js
+++ public/js/aflorya.js
@@ -12,16 +12,31 @@ async function loadProducts() {
     products.forEach(product => {
       const card = document.createElement('article');
       card.className = 'card';
+
+      // Slider için img dizisi
+      const imgs = product.imgs && product.imgs.length > 0 ? product.imgs.map(i => "/images/" + i) : [product.img || "/images/mainproduct.png"];
+      let currentImg = 0;
+
       card.innerHTML = `
-        <img src="${product.img}" alt="${product.name}">
+        <img src="${imgs[0]}" alt="${product.name}">
         <h3>${product.name}</h3>
-        <p>${product.desc}</p>
+        <p>${product.desc || ''}</p>
         <div class="meta">
-          <div class="price-small">$${product.price}</div>
+          <div class="price-small">${product.price}</div>
           <a class="whatsapp" href="javascript:openWhatsApp('${product.name}')">Order on WhatsApp</a>
         </div>
       `;
+
       grid.appendChild(card);
+
+      // Birden fazla foto varsa 2.5 saniyede bir değiştir
+      if (imgs.length > 1) {
+        const imgEl = card.querySelector('img');
+        setInterval(() => {
+          currentImg = (currentImg + 1) % imgs.length;
+          imgEl.src = imgs[currentImg];
+        }, 2500); // 2.5 saniye
+      }
     });
   } catch (err) {
     console.error('Ürünler yüklenemedi:', err);
