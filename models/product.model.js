489e5f90a06dc33012297b1f63dd4c889b9c94f0
Mahmod Masjidi <mahmod.masjidi@gmail.com>
2025-08-30 19:31:19 +0300

    foto değişimi ve bağlantı düzeltmeleri

--- models/product.model.js
+++ models/product.model.js
@@ -1,28 +1,25 @@
-// models/product.model.js
-import Product from "./Product.js"; // mongoose modelini buradan import et
+import Product from './Product.js';
 
 const adminOptions = {
-  resources: [
-    {
-      resource: Product,
-      options: {
-        properties: {
-          name: { isVisible: { list: true, filter: true, show: true, edit: true } },
-          description: { isVisible: { list: true, filter: true, show: true, edit: true } }, // desc yerine description
-          price: { isVisible: { list: true, filter: true, show: true, edit: true } },
-          img: { isVisible: { list: true, filter: true, show: true, edit: true } },
-          imgs: { isVisible: { list: false, filter: false, show: true, edit: true } },
-          category: {
-            isVisible: { list: true, filter: true, show: true, edit: true },
-            availableValues: [
-              { value: "Makeup", label: "Makeup" },
-              { value: "Cosmetic", label: "Cosmetic" }
-            ]
-          }
+  resources: [{
+    resource: Product,
+    options: {
+      properties: {
+        name: { isVisible: { list:true, filter:true, show:true, edit:true } },
+        description: { isVisible: { list:true, filter:true, show:true, edit:true } },
+        price: { isVisible: { list:true, filter:true, show:true, edit:true } },
+        img: { isVisible: { list:true, filter:true, show:true, edit:true } },
+        imgs: { isVisible: { list:false, filter:false, show:true, edit:true } },
+        category: {
+          isVisible: { list:true, filter:true, show:true, edit:true },
+          availableValues: [
+            { value: 'Makeup', label: 'Makeup' },
+            { value: 'Cosmetic', label: 'Cosmetic' }
+          ]
         }
       }
     }
-  ]
+  }]
 };
 
 export default adminOptions;
